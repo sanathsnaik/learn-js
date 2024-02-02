913106227aac4e926014c1914550352b9f25330f
sanathsnaik <naik.san@northeastern.edu>
2024-02-01 20:20:15 -0500

    quiz completed

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,13 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    if (this.errMsg) {
+      console.log(this.errMsg);
+    }
+  }
+  
+  const temp = {
+      name: 'Prajwal',
+      errMsg: 'This is an error message'
+  };
+  
+  logMsg.call(temp);
